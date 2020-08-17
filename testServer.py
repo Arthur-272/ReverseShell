0315157d5d10cbcad377dc7586f5f842ee1481d1
Radhey <radheypatel272@gmail.com>
2020-08-17 11:35:15 +0530

    added testClient and testServer

--- testServer.py
+++ testServer.py
@@ -1,4 +1,4 @@
-import pyaudio
+import cv2
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -6,29 +6,6 @@ s.bind(('',1234))
 s.listen(1)
 c, addr = s.accept()
 
-def audio(c):
-    chunk = 1024
-    FORMAT = pyaudio.paInt16
-    channels = 1
-    sample_rate = 44100
-    record_seconds = 5
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT,
-                    channels=channels,
-                    rate=sample_rate,
-                    input=True,
-                    output=True,
-                    frames_per_buffer=chunk)
-    try:
-        try:
-            while True:
-                data = c.recv(1024)
-                stream.write(data)
-        except KeyboardInterrupt:
-            pass
-    except:
-        print("Error")
-try:
-    audio(c)
-except KeyboardInterrupt:
-    pass
+def webcam():
+    while True:
+        
